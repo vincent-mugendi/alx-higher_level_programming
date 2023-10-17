e98f00f72df68b90ecab2f836ace3a446d96468e
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-10-17 16:48:11 -0700

    Corrected Task -- Displays the 3 cities with the highest average temperatures between July and August. SELECT , AVG() AS FROM WHERE  = 7 OR  = 8 GROUP BY ORDER BY  DESC LIMIT 3;

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,7 @@
--- Displays the average temperature (in Fahrenheit) by city ordered by descending temperature.
+-- Displays the 3 cities with the highest average temperatures between July and August.
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+ORDER BY `avg_temp` DESC
+LIMIT 3;
