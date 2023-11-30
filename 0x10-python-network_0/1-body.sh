5e8dbaf9e4dfe38164b2005be8dee59e7fb007da
vincent-mugendi <vincentmwathi5@gmail.com>
2023-12-01 01:26:28 +0300

    Mandatory Tasks

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sL "$1"
